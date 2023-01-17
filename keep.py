1417eed3300d6350484f8c722737a8179dc21eb1
sakrlog <sakr.log@gmail.com>
2023-01-17 11:18:03 +0200

    Added some coloring based on inverter state. and some tuya scripts

--- keep.py
+++ keep.py
@@ -3,6 +3,8 @@ import configparser
 import time
 import InverterData as Inverter
 import datetime
+import tinytuya
+
 
 configParser = configparser.RawConfigParser()
 configFilePath = r'./config.cfg'
@@ -23,6 +25,19 @@ keep = gkeepapi.Keep()
 # success = keep.login(username, 'google_apps_password')
 # token = keep.getMasterToken()
 # print(token)
+
+grid_status = 0
+
+# def manageTuya(state):
+#     d = tinytuya.OutletDevice('bfed0af475e65ecf078ier', '192.168.9.188', "6baeac8c9cf73699")
+#     d.set_version(3.3)
+#     print("entered with status")
+#     print(state)
+#     if state == 'turn_on':
+#         d.turn_on()
+#     else:
+#         d.turn_off()
+
 
 def oclock ():
     n = datetime.datetime.now()
@@ -45,13 +60,22 @@ def syncInfo(data={}):
             filled_batteries = batteries_blueprint.format(*data['batteries'])
             bnote = keep.get(batteries_notes_id)
             bnote.text = filled_batteries
+            if data['batteries'][1] == 'Charging':
+                bnote.color = gkeepapi.node.ColorValue.Green
+            else:
+                bnote.color = gkeepapi.node.ColorValue.Red
         if data['system']:
             # System
-            system_blueprint = 'Grid status: {0}.       Loads: {1} W\nTemperature: {2} C'
+            system_blueprint = 'Grid status: {0} W.       Loads: {1} W\nTemperature: {2} C.      Gen: {3} W.'
             filled_system = system_blueprint.format(*data['system'])
             snote = keep.get(system_notes_id)
             snote.text = filled_system
             snote.title = 'System  ' + data['system'][-1]
+            if data['system'][0] > 5 or data['system'][3] > 5:
+                snote.color = gkeepapi.node.ColorValue.Green
+            else:
+                snote.color = gkeepapi.node.ColorValue.Gray
+
         print("syncing")
         keep.sync()
 
@@ -62,7 +86,7 @@ while(True):
     except:
         continue
     pv = [obj['PV1 Power(W)'], obj['PV2 Power(W)'], int(obj['PV1 Power(W)'])+int(obj['PV2 Power(W)'])]
-    system = [obj['Grid Status()'], obj['Total Load Power(W)'], obj['Battery Temperature(º)'], timeNow]
+    system = [obj['Grid Status()'], obj['Total Load Power(W)'], obj['Battery Temperature(º)'],obj['Gen Power(W)'], timeNow]
     batt_status = ''
     if obj['Battery Power(W)'] > 0:
         batt_status = 'Discharging'
@@ -71,4 +95,12 @@ while(True):
     batteries = [obj['Battery Voltage(V)'], batt_status, obj['Battery Power(W)'],obj['Battery SOC(%)']]
     data = {'pv': pv, 'system': system, 'batteries': batteries}
     syncInfo(data)
+    if obj['Grid Status()'] > 5 and grid_status == 0:
+        # fi kahraba
+        manageTuya('turn_on')
+        grid_status = 1
+    elif grid_status == 1 and obj['Grid Status()'] == 0:
+        # ken fi kahraba w hala2 rahet
+        # manageTuya('turn_off')
+        grid_status = 0
     time.sleep(10)
